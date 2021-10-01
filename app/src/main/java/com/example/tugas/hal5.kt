155d8486a73ca95feaab8b1370bfec691227c212
amirul03 <mukminiamirul74@gmail.com>
2021-10-01 10:37:32 +0700

    tugas

---
+++ app/src/main/java/com/example/tugas/hal5.kt
@@ -0,0 +1,11 @@
+package com.example.tugas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class hal5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hal5)
+    }
+}
---
+++ app/src/main/java/com/example/tugas/hal6.kt
@@ -0,0 +1,11 @@
+package com.example.tugas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class hal6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hal6)
+    }
+}
